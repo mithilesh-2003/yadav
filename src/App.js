0030df70934b7fe9802f5865127812aef8483c2a
mithilesh kumar <kumaryadavmithilesh861@gmail.com>
2023-10-17 11:12:14 +0530

    practies react.js to increase my good experiance so i am practies a number of time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return(
+//     <body>
+//       <div className='app'>
+//         <div>
+        
+//       {/* <center>
+
+//           <div  style={{color:"green"}}>lists</div><hr></hr>
+//           <h2 style={{color:"red"}}>social media</h2>
+//           </center>
+//         <div className="d">
+//         <ul>
+//           <li>home</li>
+//           <li>go </li>
+//           <li>come</li>
+//           <li>back</li>
+//         </ul>
+//         </div>
+//         <div> */}
+//         <ol className="e">
+//         <li>home</li>
+//           <li>go </li>
+//           <li>come</li>
+//           <li>back</li>
+//         </ol>
+//         </div>
+//         {/* <h1>forms</h1>
+//         <form>
+//           <div className="b">
+//             <div>
+//           <lable>enter your name</lable>
+//           <input  className="a" type="text" placeholder="enter your name"></input>
+//           </div>
+//             <div>
+//           <lable>enter your name</lable>
+//           <input  className="a" type="text" placeholder="enter your name"></input>
+//           </div>
+//             <div>
+//           <lable>enter your name</lable>
+//           <input  className="a" type="text" placeholder="enter your name"></input>
+//           </div>
+//             <div>
+//           <lable>enter your name</lable>
+//           <input  className="a" type="text" placeholder="enter your name"></input>
+//           </div>
+//           </div>
+//         </form> */}
+//       </div>
+//     </body>
+//   //   <body style={{background:"green"}}>
+//   // <div className='app' style={{background:"red"}}>
+//   //   <h1>hello Digi coders</h1>
+//   //   <p classNamep2>this is a paragraph its very importent</p>
+//   //   <p className="p1">this is a paragraph its very importent</p>
+//   //   <p className="p3">this is a paragraph its very importent</p>
+//   //   <button className="b1" type='button'>submit</button>
+//   //   <input className="b2" type="text" placeholder="enter name"></input>
+//   // </div>
+//   // </body>
+//   );
+// }
+
+// export default App;
 import './App.css';
 
-function App() {
+
+function components() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{height:"400px", width:"1000px", background:"green",borderRadius:"10px"}}>
+      <div className='card1' style={{height:"200px", width:"200px", background:"white", position:'absolute', top:"40px", left:"20px",borderRadius:"10px",boxShadow:"10px",cursor:"pointer",}}></div>
+      <div className='card1' style={{height:"200px", width:"200px", background:"white", position:'absolute', top:"40px", left:"270px",borderRadius:"10px",cursor:"pointer"}}></div>
+      <div className='card1' style={{height:"200px", width:"200px", background:"white", position:'absolute', top:"40px", left:"520px",borderRadius:"10px",cursor:"pointer"}}></div>
+      <div className='card1' style={{height:"200px", width:"200px", background:"white", position:'absolute', top:"40px", left:"760px",borderRadius:"10px",cursor:"pointer"}}></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default components
+
---
+++ src/components.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function components() {
+  return (
+    <div style={{height:"400px", width:"800px", background:"green"}}>
+      
+    </div>
+  )
+}
+
+export default components
